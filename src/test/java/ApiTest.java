2da493ee1602829468665dcc48caf26f9139a4d2
Hirosh Tharaka <ehirosht@gmail.com>
2019-05-10 14:58:46 +0530

    automationtesting.com code added

--- src/test/java/ApiTest.java
+++ src/test/java/ApiTest.java
@@ -1,8 +1,12 @@
+
 import io.restassured.RestAssured;
 import io.restassured.config.RestAssuredConfig;
+import io.restassured.response.Response;
 import org.junit.Test;
 
+import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class ApiTest {
@@ -10,8 +14,17 @@ public class ApiTest {
 
     //example as given on https://automationintesting.com/restassured/lessons/setup/
     @Test
-    public void myFirstRaTest(){
+    public void myFirstRaTest() {
         assertThat(RestAssured.config(), instanceOf(RestAssuredConfig.class));
+    }
+
+    //example as given on https://automationintesting.com/restassured/lessons/request/
+    @Test
+    public void getTest() {
+        Response response = given().get("https://restful-booker.herokuapp.com/ping/");
+        int statusCode = response.getStatusCode();
+
+        assertThat(statusCode, is(201));
     }
 
 }
